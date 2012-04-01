6816e7ca43ef6d46e860358c1c558128b68f0c2d
Steven Heidel <steven@livingskyweb.ca>
2012-04-01 17:25:22 -0600

    More work completed on the Place class.

--- src/utilities/Place.java
+++ src/utilities/Place.java
@@ -8,8 +8,38 @@ public class Place
 	
 	public Place(String theCountry, String theCity)
 	{
+		city = theCity;
 		
+		if (country == null)
+			country = findCountry(theCity);
+		else
+			country = theCountry;
 	}
 	
+	public Place(String unknown)
+	{
+		// determine if city or country
+	}
 	
+	private String findCountry(String theCity)
+	{
+		// search the CSV file
+		
+		return "Japan";
+	}
+	
+	public boolean hasCity()
+	{
+		return (city != null);
+	}
+	
+	public String getCity()
+	{
+		return city;
+	}
+	
+	public String getCountry()
+	{
+		return country;
+	}
 }
