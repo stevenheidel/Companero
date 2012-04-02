ac40b8e6ec4e0d66717529bf799d319ce2a53dc7
Steven Heidel <steven@livingskyweb.ca>
2012-04-01 19:49:41 -0600

    Completed implementation of Place class

--- src/utilities/Place.java
+++ src/utilities/Place.java
@@ -1,31 +1,81 @@
 package utilities;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Place
 {
 	private String city;
 	
 	private String country;
 	
-	public Place(String theCountry, String theCity)
+	private static HashMap<String, String> cities = null;
+	
+	private static HashSet<String> countries = null;
+	
+	public Place(String theCity, String theCountry)
 	{
-		city = theCity;
-		
-		if (country == null)
-			country = findCountry(theCity);
-		else
-			country = theCountry;
+		initialize(theCity, theCountry);
 	}
 	
 	public Place(String unknown)
 	{
-		// determine if city or country
+		buildStructures();
+		
+		unknown = unknown.toUpperCase();
+		
+		if (countries.contains(unknown))
+			initialize(null, unknown);
+		else
+			initialize(unknown, null);
+	}
+	
+	private void initialize(String theCity, String theCountry)
+	{
+		if (theCity == null)
+			city = null;
+		else
+			city = theCity.toUpperCase();
+		
+		if (theCountry == null)
+			country = findCountry(city);
+		else
+			country = theCountry.toUpperCase();
 	}
 	
 	private String findCountry(String theCity)
 	{
-		// search the CSV file
+		buildStructures();
 		
-		return "Japan";
+		return cities.get(theCity);
+	}
+	
+	private void buildStructures()
+	{
+		if (cities != null && countries != null)
+			return;
+		
+		cities = new HashMap<String, String>();
+		countries = new HashSet<String>();
+		
+		Scanner scanner = null;
+		
+		try {
+			scanner = new Scanner(new File("cities_countries.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		while (scanner.hasNextLine())
+		{
+			String[] line = scanner.nextLine().split("\\|");
+			
+			cities.put(line[0].toUpperCase(), line[1].toUpperCase());
+			countries.add(line[1].toUpperCase());
+		}
 	}
 	
 	public boolean hasCity()
@@ -41,5 +91,30 @@ public class Place
 	public String getCountry()
 	{
 		return country;
+	}
+	
+	public String toString()
+	{
+		if (hasCity())
+			return city + ", " + country;
+		else
+			return country;
+	}
+	
+	public static void main(String[] args)
+	{
+		Place both = new Place("Saskatoon", "Canada");
+		Place city = new Place("Saskatoon", null);
+		Place country = new Place(null, "Canada");
+		Place unknown1 = new Place("Saskatoon");
+		Place unknown2 = new Place("Canada");
+		Place fake = new Place("Fake");
+		
+		System.out.println(both);
+		System.out.println(city);
+		System.out.println(country);
+		System.out.println(unknown1);
+		System.out.println(unknown2);
+		System.out.println(fake);
 	}
 }
