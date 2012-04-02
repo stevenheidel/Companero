8fd7fc823a43f59bf8585b9d58435df22d4cd8da
Steven Heidel <steven@livingskyweb.ca>
2012-04-02 15:04:31 -0600

    Clean up file reader.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,8 @@
 package main;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
@@ -17,51 +15,28 @@ public class Main
 {
 	public static String readTextFile(String fullPathFilename) throws IOException 
 	{
-		/*
-		String toReturn;
-		
-		StringBuffer sb = new StringBuffer(1024);
-		BufferedReader reader = new BufferedReader(new FileReader(fullPathFilename));
-				
-		char[] chars = new char[1024];
-		while (reader.read(chars) > -1)
-		{
-			sb.append(String.valueOf(chars));	
-		}
-
-		reader.close();
-		
-		toReturn = sb.toString();
-		
-		//System.out.print(toReturn);
-
-		return toReturn;
-		*/
-		
 		String toReturn = "";
 		
-		FileInputStream corpusFile;
-		BufferedReader corpusReader;
+		FileInputStream fis;
+		BufferedReader br;
 		
 		try
 		{
-			corpusFile = new FileInputStream(fullPathFilename);
-			corpusReader = new BufferedReader(new InputStreamReader(corpusFile));
+			fis = new FileInputStream(fullPathFilename);
+			br = new BufferedReader(new InputStreamReader(fis));
 			
-			String nextLine = corpusReader.readLine();
-			while(nextLine != null)
+			String nextLine = br.readLine();
+			while (nextLine != null)
 			{
 				toReturn += nextLine;
 				toReturn += "\n";
-				nextLine = corpusReader.readLine();
+				nextLine = br.readLine();
 			}
 		}
 		catch (FileNotFoundException e)
 		{
 			throw new IOException("File not found");
 		}
-		
-		//System.out.print(toReturn);
 		
 		return toReturn;
 	}
