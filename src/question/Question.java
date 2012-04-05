d7a2ace3a512fed3bce066c6d92255ab2c35228c
Jamie Gaultois <heartbeat_of_america@hotmail.com>
2012-04-04 21:42:24 -0600

    Missed the java file on the last commit

--- src/question/Question.java
+++ src/question/Question.java
@@ -1,8 +1,16 @@
 package question;
 
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import main.Main;
+
+import utilities.Place;
+
+import corpus.Article;
+import corpus.Corpus;
 
 public class Question
 {
@@ -64,8 +72,123 @@ public class Question
 		return time;
 	}
 	
+	public String whenAnswer(Corpus corpus)
+	{
+		// Get the list of articles with the text required
+		LinkedList<Article> articles = corpus.getArticlesWithText(text);
+		
+		String returnDate = "No date found";
+		
+		Place questionPlace = new Place(place);
+		for(Article a : articles)
+		{
+			// FIX THIS!!!!!!!
+			Place articlePlace  = a.getLocationWritten();
+			if(articlePlace.getCountry().equals(questionPlace.getCountry()))
+			{
+				if(questionPlace.hasCity()
+						&& !questionPlace.getCity().equals(articlePlace.getCity()))
+				{
+					continue;
+				}
+				
+				LinkedList<String> dates = a.getDatesFromText();
+				
+				int minDistance = Integer.MAX_VALUE;
+				
+				for(String s : dates)
+				{
+					int dist = a.closenessOfDateToText(s, text);
+					if(dist < minDistance)
+					{
+						minDistance = dist;
+						returnDate = s.replace("(yearadded)", "");
+					}
+				}
+				
+				// If we didn't find a date in the text, give the date the article was written
+				if(returnDate.equals("No date found"))
+				{
+					returnDate = a.getDateWritten().toString();
+				}
+				
+				break;
+			}
+		}
+		
+		return returnDate;
+	}
+	
+	public String whatAnswer(Corpus corpus)
+	{
+		String answer = "Text not found";
+		
+		// Get the list of articles with the text required
+		LinkedList<Article> articles = corpus.getArticlesWithText(text);
+		
+		Place questionPlace = new Place(place);
+		for(Article a : articles)
+		{
+			// FIX THIS!!!!!!!
+			Place articlePlace  = a.getLocationWritten();
+			if(articlePlace.getCountry().equals(questionPlace.getCountry()))
+			{
+				if(questionPlace.hasCity()
+						&& !questionPlace.getCity().equals(articlePlace.getCity()))
+				{
+					continue;
+				}
+				
+				// Get the rest of the sentence after the text
+				int index = a.getArticleText().indexOf(text) + text.length();
+				
+				answer = a.getArticleText().substring(index);
+				
+				String tempAnswer = answer.split("\\.")[0];
+				if(tempAnswer.contains("\""))
+				{
+					String[] answerSplit = answer.split("\"");
+					answer = answerSplit[0] + "\"" + answerSplit[1] + "\"";
+				}
+				else
+				{
+					answer = answer.split("\\.")[0];
+				}
+				
+				// We found a correct answer, so break
+				break;
+			}
+		}
+		
+		return answer;		
+	}
+	
+	public String answer(Corpus corpus)
+	{
+		if(type.equals("WHEN"))
+		{
+			return whenAnswer(corpus);
+		}
+		else if(type.equals("WHAT"))
+		{
+			return whatAnswer(corpus);
+		}
+		return "";
+	}
+	
 	public static void main(String[] args)
 	{
+		Corpus corpus = null;
+		try
+		{
+			corpus = new Corpus(Main.readTextFile("corpus.txt"));
+		} 
+		catch (Exception e)
+		{
+			System.out.println("Error reading the corpus");
+			System.exit(-1);
+		}
+		
 		Question who = new Question("Who was [the army judge advocate general] of [Chile] in [1990]?");
 		Question what = new Question("What is [an example Torres cited] in [Chile] in [1990]?");
 		Question where = new Question("Where is it that [the government junta yesterday approved a draft bill] on [5 Jan 90]?");
@@ -75,20 +198,24 @@ public class Question
 		System.out.println(who.getText());
 		System.out.println(who.getPlace());
 		System.out.println(who.getTime());
+		System.out.println(who.answer(corpus));
 		
 		System.out.println(what.getType());
 		System.out.println(what.getText());
 		System.out.println(what.getPlace());
 		System.out.println(what.getTime());
+		System.out.println(what.answer(corpus));
 		
 		System.out.println(where.getType());
 		System.out.println(where.getText());
 		System.out.println(where.getPlace());
 		System.out.println(where.getTime());
+		System.out.println(where.answer(corpus));
 		
 		System.out.println(when.getType());
 		System.out.println(when.getText());
 		System.out.println(when.getPlace());
 		System.out.println(when.getTime());
+		System.out.println(when.answer(corpus));
 	}
 }
